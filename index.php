33c7208589ec50cded3e32044d32ad5b42566005
cunshanggangg <cunshanggangg@gmail.com>
2018-01-04 14:42:52 +0800

    Add index.

--- index.php
+++ index.php
@@ -1,40 +1,44 @@
 <?php
+//定义常量token
 
-include_once "wxBizMsgCrypt.php";
+define('TOKEN','james');
 
-// 第三方发送消息给公众平台
-$encodingAesKey = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFG";
-$token = "pamtest";
-$timeStamp = "1409304348";
-$nonce = "xxxxxx";
-$appId = "wxb11529c136998cb6";
-$text = "<xml><ToUserName><![CDATA[oia2Tj我是中文jewbmiOUlr6X-1crbLOvLw]]></ToUserName><FromUserName><![CDATA[gh_7f083739789a]]></FromUserName><CreateTime>1407743423</CreateTime><MsgType><![CDATA[video]]></MsgType><Video><MediaId><![CDATA[eYJ1MbwPRJtOvIEabaxHs7TX2D-HV71s79GUxqdUkjm6Gs2Ed1KF3ulAOA9H1xG0]]></MediaId><Title><![CDATA[testCallBackReplyVideo]]></Title><Description><![CDATA[testCallBackReplyVideo]]></Description></Video></xml>";
+//检查标签
+function checkSignature()
+{
+    //先获取到这三个参数
+    $signature = $_GET['signature'];
+    $nonce = $_GET['nonce'];
+    $timestamp = $_GET['timestamp'];
 
+    //把这三个参数存到一个数组里面
+    $tmpArr = array($timestamp,$nonce,TOKEN);
+    //进行字典排序
+    sort($tmpArr);
 
-$pc = new WXBizMsgCrypt($token, $encodingAesKey, $appId);
-$encryptMsg = '';
-$errCode = $pc->encryptMsg($text, $timeStamp, $nonce, $encryptMsg);
-if ($errCode == 0) {
-	print("加密后: " . $encryptMsg . "\n");
-} else {
-	print($errCode . "\n");
+    //把数组中的元素合并成字符串，impode()函数是用来将一个数组合并成字符串的
+    $tmpStr = implode($tmpArr);
+
+    //sha1加密，调用sha1函数
+    $tmpStr = sha1($tmpStr);
+    //判断加密后的字符串是否和signature相等
+    if($tmpStr == $signature)
+    {
+
+        return true;
+    }
+    return false;
+}
+//如果相等，验证成功就返回echostr
+if(checkSignature())
+{
+    //返回echostr
+    $echostr = $_GET['echostr'];
+    if($echostr)
+    {
+        echo $echostr;
+        exit;
+    }
 }
 
-$xml_tree = new DOMDocument();
-$xml_tree->loadXML($encryptMsg);
-$array_e = $xml_tree->getElementsByTagName('Encrypt');
-$array_s = $xml_tree->getElementsByTagName('MsgSignature');
-$encrypt = $array_e->item(0)->nodeValue;
-$msg_sign = $array_s->item(0)->nodeValue;
-
-$format = "<xml><ToUserName><![CDATA[toUser]]></ToUserName><Encrypt><![CDATA[%s]]></Encrypt></xml>";
-$from_xml = sprintf($format, $encrypt);
-
-// 第三方收到公众号平台发送的消息
-$msg = '';
-$errCode = $pc->decryptMsg($msg_sign, $timeStamp, $nonce, $from_xml, $msg);
-if ($errCode == 0) {
-	print("解密后: " . $msg . "\n");
-} else {
-	print($errCode . "\n");
-}
+?>
