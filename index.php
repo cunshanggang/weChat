13030ede2c9b0e6de531fcac74f64fdb30195f55
cunshanggangg <cunshanggangg@gmail.com>
2018-01-08 15:16:32 +0800

    Fourteenth times.

--- index.php
+++ index.php
@@ -58,10 +58,10 @@ class wechatCallbackapiTest
                 $contentStr = "Welcome to wechat world!";
                 $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
                 echo $resultStr;
-                exit();
+                exit;
             }else{
                 echo "Input something...";
-                exit();
+                exit;
             }
         }
 //        else {
