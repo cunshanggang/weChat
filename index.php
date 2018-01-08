c6c1c4637242fb2d356a4841290857adeeec36a7
cunshanggang <cunshanggang@163.com>
2018-01-08 22:39:56 +0800

    Eighteenth times.

--- index.php
+++ index.php
@@ -2,97 +2,187 @@
 /**
  * Created by PhpStorm.
  * User: Administrator
- * Date: 2018/1/5
- * Time: 15:44
+ * Date: 2018/1/8
+ * Time: 22:38
  */
-/**
- * wechat php test
- */
+header('Content-type:text');
+define("TOKEN", "weixin");
 
-//define your token
-define("TOKEN", "james");
 $wechatObj = new wechatCallbackapiTest();
-//$wechatObj->valid();
-//exit;
-$wechatObj->responseMsg();
+if (!isset($_GET['echostr'])) {
+    $wechatObj->responseMsg();
+}else{
+    $wechatObj->valid();
+}
 
 class wechatCallbackapiTest
 {
     public function valid()
     {
         $echoStr = $_GET["echostr"];
-
-        //valid signature , option
         if($this->checkSignature()){
-//            return $echoStr;
-            //echo $echoStr;
-            //exit;
-        }
-    }
-
-    public function responseMsg()
-    {
-        //get post data, May be due to the different environments
-        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-
-        //extract post data
-        if (!empty($postStr)){
-            /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
-               the best way is to check the validity of xml by yourself */
-            libxml_disable_entity_loader(true);
-            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-            $fromUsername = $postObj->FromUserName;
-            $toUsername = $postObj->ToUserName;
-            $keyword = trim($postObj->Content);
-            $time = time();
-            $textTpl = "<xml>  
-    <ToUserName><![CDATA[%s]]></ToUserName>  
-    <FromUserName><![CDATA[%s]]></FromUserName>  
-    <CreateTime>%s</CreateTime>  
-    <MsgType><![CDATA[%s]]></MsgType>  
-    <Content><![CDATA[%s]]></Content>  
-    <FuncFlag>0</FuncFlag>  
-</xml>";
-            if(!empty( $keyword ))
-            {
-                $msgType = "text";
-                $contentStr = "Welcome to wechat world!";
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                return $resultStr;
-                //exit;
-            }else{
-                return "Input something...";
-                //exit;
-            }
-        }
-        else {
-            return "";
-            //exit;
+            echo $echoStr;
+            exit;
         }
     }
 
     private function checkSignature()
     {
-        // you must define TOKEN by yourself
-        if (!defined("TOKEN")) {
-            throw new Exception('TOKEN is not defined!');
-        }
-
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
         $nonce = $_GET["nonce"];
-
         $token = TOKEN;
         $tmpArr = array($token, $timestamp, $nonce);
-        // use SORT_STRING rule
-        sort($tmpArr, SORT_STRING);
-        $tmpStr = implode( $tmpArr );
-        $tmpStr = sha1( $tmpStr );
+        sort($tmpArr);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
 
-        if( $tmpStr == $signature ){
+        if($tmpStr == $signature){
             return true;
         }else{
             return false;
+        }
+    }
+
+    public function responseMsg()
+    {
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+        if (!empty($postStr)){
+            $this->logger("R ".$postStr);
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $RX_TYPE = trim($postObj->MsgType);
+
+            switch ($RX_TYPE)
+            {
+                case "event":
+                    $result = $this->receiveEvent($postObj);
+                    break;
+                case "text":
+                    $result = $this->receiveText($postObj);
+                    break;
+            }
+            $this->logger("T ".$result);
+            echo $result;
+        }else {
+            echo "";
+            exit;
+        }
+    }
+
+    private function receiveEvent($object)
+    {
+        $content = "";
+        switch ($object->Event)
+        {
+            case "subscribe":
+                $content = "欢迎关注方倍工作室";
+                break;
+            case "unsubscribe":
+                $content = "取消关注";
+                break;
+        }
+        $result = $this->transmitText($object, $content);
+        return $result;
+    }
+
+    //接收文本消息
+    private function receiveText($object)
+    {
+        $keyword = trim($object->Content);
+        $content = date("Y-m-d H:i:s",time())."\n技术支持 方倍工作室";
+
+        if(is_array($content)){
+            if (isset($content[0]['PicUrl'])){
+                $result = $this->transmitNews($object, $content);
+            }else if (isset($content['MusicUrl'])){
+                $result = $this->transmitMusic($object, $content);
+            }
+        }else{
+            $result = $this->transmitText($object, $content);
+        }
+
+        return $result;
+    }
+
+
+    private function transmitText($object, $content)
+    {
+        $textTpl = "<xml>
+<ToUserName><![CDATA[%s]]></ToUserName>
+<FromUserName><![CDATA[%s]]></FromUserName>
+<CreateTime>%s</CreateTime>
+<MsgType><![CDATA[text]]></MsgType>
+<Content><![CDATA[%s]]></Content>
+</xml>";
+        $result = sprintf($textTpl, $object->FromUserName, $object->ToUserName, time(), $content);
+        return $result;
+    }
+
+    private function transmitNews($object, $arr_item)
+    {
+        if(!is_array($arr_item))
+            return;
+
+        $itemTpl = "    <item>
+        <Title><![CDATA[%s]]></Title>
+        <Description><![CDATA[%s]]></Description>
+        <PicUrl><![CDATA[%s]]></PicUrl>
+        <Url><![CDATA[%s]]></Url>
+    </item>
+";
+        $item_str = "";
+        foreach ($arr_item as $item)
+            $item_str .= sprintf($itemTpl, $item['Title'], $item['Description'], $item['PicUrl'], $item['Url']);
+
+        $newsTpl = "<xml>
+<ToUserName><![CDATA[%s]]></ToUserName>
+<FromUserName><![CDATA[%s]]></FromUserName>
+<CreateTime>%s</CreateTime>
+<MsgType><![CDATA[news]]></MsgType>
+<Content><![CDATA[]]></Content>
+<ArticleCount>%s</ArticleCount>
+<Articles>
+$item_str</Articles>
+</xml>";
+
+        $result = sprintf($newsTpl, $object->FromUserName, $object->ToUserName, time(), count($arr_item));
+        return $result;
+    }
+
+    private function transmitMusic($object, $musicArray)
+    {
+        $itemTpl = "<Music>
+    <Title><![CDATA[%s]]></Title>
+    <Description><![CDATA[%s]]></Description>
+    <MusicUrl><![CDATA[%s]]></MusicUrl>
+    <HQMusicUrl><![CDATA[%s]]></HQMusicUrl>
+</Music>";
+
+        $item_str = sprintf($itemTpl, $musicArray['Title'], $musicArray['Description'], $musicArray['MusicUrl'], $musicArray['HQMusicUrl']);
+
+        $textTpl = "<xml>
+<ToUserName><![CDATA[%s]]></ToUserName>
+<FromUserName><![CDATA[%s]]></FromUserName>
+<CreateTime>%s</CreateTime>
+<MsgType><![CDATA[music]]></MsgType>
+$item_str
+</xml>";
+
+        $result = sprintf($textTpl, $object->FromUserName, $object->ToUserName, time());
+        return $result;
+    }
+
+    private function logger($log_content)
+    {
+        if(isset($_SERVER['HTTP_APPNAME'])){   //SAE
+            sae_set_display_errors(false);
+            sae_debug($log_content);
+            sae_set_display_errors(true);
+        }else if($_SERVER['REMOTE_ADDR'] != "127.0.0.1"){ //LOCAL
+            $max_size = 10000;
+            $log_filename = "log.xml";
+            if(file_exists($log_filename) and (abs(filesize($log_filename)) > $max_size)){unlink($log_filename);}
+            file_put_contents($log_filename, date('H:i:s')." ".$log_content."\r\n", FILE_APPEND);
         }
     }
 }
