c1d26e11c2c567753c7480e829ab924a50a839e2
cunshanggangg <cunshanggangg@gmail.com>
2018-01-09 17:45:52 +0800

    Twenty-sixth times. file_get_contents("php://input")

--- index.php
+++ index.php
@@ -32,7 +32,8 @@ class wechatCallbackapiTest
     public function responseMsg()
     {
         //get post data, May be due to the different environments
-        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+//        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+        $postStr = file_get_contents("php://input");
 
         //extract post data
         if (!empty($postStr)){
