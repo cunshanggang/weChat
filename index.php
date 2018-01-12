0bb572237a19f49b439ae763f01acbdda2c5d390
cunshanggangg <cunshanggangg@gmail.com>
2018-01-12 10:43:35 +0800

    Optimize code.

--- index.php
+++ index.php
@@ -15,10 +15,9 @@ $wechatObj = new wechatCallbackapiTest();
 //exit;
 $wechatObj->responseMsg();
 
-class wechatCallbackapiTest
-{
-    public function valid()
-    {
+class wechatCallbackapiTest {
+    //验证token
+    public function valid() {
         $echoStr = $_GET["echostr"];
 
         //valid signature , option
@@ -28,8 +27,7 @@ class wechatCallbackapiTest
         }
     }
 
-    public function responseMsg()
-    {
+    public function responseMsg() {
         //get post data, May be due to the different environments
         $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
         //ini_set('always_populate_raw_post_data',-1);
@@ -44,70 +42,54 @@ class wechatCallbackapiTest
             $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
             $fromUsername = $postObj->FromUserName;
             $toUsername = $postObj->ToUserName;
-            $msgType = $postObj->MsgType;//消息类型：事件或者文本
+//            $msgType = $postObj->MsgType;//消息类型：事件或者文本
             $event = $postObj->Event;//事件类型，subscribe(订阅),unsubscribe(取消订阅)
             $keyword = trim($postObj->Content);
             $time = time();
-            $textTpl = "<xml>  
-<ToUserName><![CDATA[%s]]></ToUserName>  
-<FromUserName><![CDATA[%s]]></FromUserName>  
-<CreateTime>%s</CreateTime>  
-<MsgType><![CDATA[%s]]></MsgType>  
-<Content><![CDATA[%s]]></Content>
-<FuncFlag>0</FuncFlag>
-</xml>";
-            if(!empty( $keyword ))
-            {
-                $msgType = "text";
-                if($keyword == "姚明") {
-                    $contentStr = "姚明！要命！饶命！".$this->emoji($emoji_str = "/::)");
-                }else{
-                    preg_match("/(\d+)([+-])(\d+)/i",$keyword,$res);
-                    switch ($res[2]) {
-                        case '+';
-                            $result = $res[1]+$res[3];
-                            break;
-                        case '-';
-                            $result = $res[1]-$res[3];
-                            break;
-                    }
-//                    if($res[2] == '+') {
-//                        $result = $res[1]+$res[3];
-//                    }else if($res[2] == '-') {
-//                        $result = $res[1]-$res[3];
-//                    }
-                    $contentStr = "运算结果是:".$result;
-                }
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                echo $resultStr;
-                exit;
-            }else if($msgType) {
-                //关注subscribe,取消关注unsubscribe
-                switch ($msgType) {
-                    case "event":
-                        if($event == 'subscribe') {
-                            $contentStr = "Hi,欢迎关注jamess公众号，期待您的到来!";
-                        }
+
+            if(!empty($keyword)) {
+                switch ($keyword) {
+                    case "姚明":
+                        $contentStr = "姚明！要命！饶命！".$this->emoji($emoji_str = "/::)");
+                        $msgType = "text";
+                        $resultStr = sprintf($this->textTpl(), $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                        echo $resultStr;
+                        break;
+                    default:
+                        $contentStr = "请输入关键字";
+                        $msgType = "text";
+                        $resultStr = sprintf($this->textTpl(), $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                        echo $resultStr;
                         break;
                 }
+            }
+            ////关注subscribe,取消关注unsubscribe
+            if($event == 'subscribe') {
+                $contentStr = "Hi,欢迎关注jamess公众号，期待您的到来!";
                 $msgType = 'text';
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                $resultStr = sprintf($this->textTpl(), $fromUsername, $toUsername, $time, $msgType, $contentStr);
                 echo $resultStr;
-                exit;
-            } else{
-                echo "Input something...";
                 exit;
             }
 
-        }
-        else {
+        } else {
             echo "";
             exit;
         }
     }
 
-    private function checkSignature()
-    {
+    public function textTpl() {
+        $textTpl = "<xml>  
+                    <ToUserName><![CDATA[%s]]></ToUserName>  
+                    <FromUserName><![CDATA[%s]]></FromUserName>  
+                    <CreateTime>%s</CreateTime>  
+                    <MsgType><![CDATA[%s]]></MsgType>  
+                    <Content><![CDATA[%s]]></Content>
+                    <FuncFlag>0</FuncFlag>
+                    </xml>";
+        return $textTpl;
+    }
+    private function checkSignature() {
         // you must define TOKEN by yourself
         if (!defined("TOKEN")) {
             throw new Exception('TOKEN is not defined!');
@@ -132,8 +114,7 @@ class wechatCallbackapiTest
     }
 
     //解析表情
-    public function emoji($emoji_str)
-    {
+    public function emoji($emoji_str) {
         //将字符串组合成json格式
         $emoji_str = '["'.$emoji_str.'"]';
         $emoji_arr = json_decode($emoji_str, true);
