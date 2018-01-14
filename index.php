a560dc69726fbd41033c2e677599569b633233a7
cunshanggang <cunshanggang@163.com>
2018-01-14 15:57:45 +0800

    The picture isn't display.

--- index.php
+++ index.php
@@ -73,7 +73,7 @@ class wechatCallbackapiTest {
                         echo $resultStr;
                         break;
                     default:
-                        $contentStr = "亲,请输入关键字哦".$this->emoji($emoji_str = "/::D)");
+                        $contentStr = "亲,请输入关键字哦".$this->emoji($emoji_str = "/::D");
                         $msgType = "text";
                         $resultStr = sprintf($this->textTpl(), $fromUsername, $toUsername, $time, $msgType, $contentStr);
                         echo $resultStr;
