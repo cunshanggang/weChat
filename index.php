209a3f6978ae0ccf6a4239daf4b184f92b168203
cunshanggangg <cunshanggangg@gmail.com>
2018-01-18 10:23:22 +0800

    Baidu weather.

--- index.php
+++ index.php
@@ -202,7 +202,8 @@ class wechatCallbackapiTest {
                         //合并
                         $xml = $contentHead.$contentBody.$contentFooter;
 //                        file_put_contents("error.log",$xml.PHP_EOL,FILE_APPEND);
-                        $resultStr = sprintf($xml, $fromUsername, $toUsername, $time, count($result['results'][0]['weather_data'])+1);
+                        $c = count($result['results'][0]['weather_data'])+1;
+                        $resultStr = sprintf($xml, $fromUsername, $toUsername, $time, $c);
                         echo $resultStr;
                         break;
                 }
