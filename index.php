9d28539ce4fe5bcd8aed6cc61f1707c7cd15a741
cunshanggangg <cunshanggangg@gmail.com>
2018-01-18 09:39:52 +0800

    Optimize code.

--- index.php
+++ index.php
@@ -155,7 +155,8 @@ class wechatCallbackapiTest {
                         echo $resultStr;
                         break;
                     case 'cxtq':
-                        $res = $this->cURL($r[2]);
+                        $url = "http://www.sojson.com/open/api/weather/json.shtml?city=$r[2]";
+                        $res = $this->cURL($url);
                         $contentStr = "昨天:\n".$res['data']['yesterday']['date']."\n".'日出:'.$res['data']['yesterday']['sunrise']."\n".'最高温:'.$res['data']['yesterday']['high']."\n".'最低温:'.$res['data']['yesterday']['low']."\n".'日落:'.$res['data']['yesterday']['sunset']."\n".'空气质量:'.$res['data']['yesterday']['aqi']."\n".'风向:'.$res['data']['yesterday']['fx']."\n".'风级:'.$res['data']['yesterday']['fl']."\n".'类型:'.$res['data']['yesterday']['type']."\n".'温馨提示:'.$res['data']['yesterday']['notice']."\n\r";
                         foreach($res['data']['forecast'] as $k=>$v) {
                             $contentStr.= $v['date']."\n".'日出:'.$v['sunrise']."\n".'最高温:'.$v['high']."\n".'最低温:'.$v['low']."\n".'日落:'.$v['sunset']."\n".'空气质量:'.$v['aqi']."\n".'风向:'.$v['fx']."\n".'风级:'.$v['fl']."\n".'类型:'.$v['type']."\n".'温馨提示:'.$v['notice']."\n\r";
@@ -302,9 +303,8 @@ class wechatCallbackapiTest {
     }
 
     //cURL
-    public function cURL($area) {
+    public function cURL($url) {
         $chjk = curl_init('http://404.php.net/');//初始化一个curl会话
-        $url = "http://www.sojson.com/open/api/weather/json.shtml?city=$area";
         curl_setopt($chjk,CURLOPT_URL,$url);//设置curl会话的接口地址
         curl_setopt($chjk,CURLOPT_CUSTOMREQUEST,"GET");//设置请求方式为GET
         curl_setopt($chjk,CURLOPT_RETURNTRANSFER,1);//设置CURLOPT_RETURNTRANSFER为1，表示如果成功只将结果返回，不自动输出任何内容。如果失败返回FALSE
