93f7a7845cb3513f887e062405ae38f5258fc9fe
cunshanggang <cunshanggang@163.com>
2018-08-12 19:13:30 +0800

    Compared the time.

--- crontab/updateUndercover.php
+++ crontab/updateUndercover.php
@@ -6,4 +6,14 @@
  * Time: 13:42
  */
 require_once '../app/database.php';
-$GLOBALS['database']->update("undercover",["status"=>0],["status"=>1]);
+//获取undercover的id
+$uid       = $GLOBALS['database']->select("undercover",["id"],["status"=>1]);
+//获取第一个发起游戏的人的时间
+$startTime = $GLOBALS['database']->select("player",["time"],["u_id"=>"$uid","order"=>1]);
+//获取当前时间
+$time      = time();
+//十分钟更新
+$deadline  = strtotime($startTime) + 3600*10;
+if($time > $deadline) {
+    $GLOBALS['database']->update("undercover",["status"=>0],["status"=>1]);
+}
