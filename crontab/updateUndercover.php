9b23f9c1a4b3b7213e6967ae56a7d810ae41816c
cunshanggang <cunshanggang@163.com>
2018-08-12 19:26:00 +0800

    Add an index  to an array.

--- crontab/updateUndercover.php
+++ crontab/updateUndercover.php
@@ -7,16 +7,19 @@
  */
 require_once '../app/database.php';
 //获取undercover的id
-$uid       = $GLOBALS['database']->select("undercover",["id"],["status"=>1]);
-echo "<pre>";
-print_r($uid);
+$result       = $GLOBALS['database']->select("undercover",["id"],["status"=>1]);
+$uid          = $result[0]['id'];
+unset($result);
 //获取第一个发起游戏的人的时间
-$startTime = $GLOBALS['database']->select("player",["time"],["u_id"=>"$uid","order"=>1]);
-print_r($startTime);
+$result       = $GLOBALS['database']->select("player",["time"],["u_id"=>"$uid","order"=>1]);
+$startTime    = $result[0]['time'];
 //获取当前时间
-$time      = time();
+$time         = time();
+echo $startTime;
+echo "<hr>";
 //十分钟更新
-$deadline  = strtotime($startTime) + 3600*10;
+$deadline     = strtotime($startTime) + 3600*10;
+echo $deadline;
 if($time > $deadline) {
     $GLOBALS['database']->update("undercover",["status"=>0],["status"=>1]);
 }
