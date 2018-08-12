0c0563beb34e6c9f2d74288712eedeff5b874b4e
cunshanggang <cunshanggang@163.com>
2018-08-12 13:56:43 +0800

    Make a crontab in undercover table.

---
+++ crontab/updataUndercover.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/12
+ * Time: 13:42
+ */
+require_once 'app/database.php';
+$GLOBALS['database']->update("undercover",["status"=>0],["status"=>1]);
